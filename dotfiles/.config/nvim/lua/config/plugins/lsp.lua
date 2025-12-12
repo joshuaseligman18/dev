65c3837297db5cedfb33b8d4f45fe367c7f7c57f
Joshua Seligman <joshuaseligman18@gmail.com>
2025-12-12 00:41:49 -0500

    Got nvim api autocomplete and added trouble plugin

--- dotfiles/.config/nvim/lua/config/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/config/plugins/lsp.lua
@@ -1,5 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
+    tag = "v2.5.0",
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         {
@@ -18,18 +19,16 @@ return {
         vim.api.nvim_create_autocmd('LspAttach', {
             group = lspKeymapGroup,
             callback = function(event)
-                local opts = {buffer = event.buf}
+                local opts = { buffer = event.buf }
 
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
                 vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
                 vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
                 vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1, float = false }) end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = -1, float = false }) end, opts)
             end
         })
 
@@ -50,14 +49,11 @@ return {
         })
 
         vim.diagnostic.config({
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
+            virtual_text = false,
+            virtual_lines = { current_line = false },
+            underline = true,
+            update_in_insert = true,
+            float = false,
         })
     end,
 }
---
+++ dotfiles/.config/nvim/lua/config/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/trouble.nvim",
+    tag = "v3.7.1",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle focus=true<cr>",
+            desc = "Trouble Diagnostics",
+        },
+    },
+}
